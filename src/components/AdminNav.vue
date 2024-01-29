e011b08676efac16815034a2bc7c27f3f75d11e8
anthony krinsky <anthony.krinsky@databricks.com>
2024-01-28 23:48:28 -0800

    Updated pages

--- src/components/AdminNav.vue
+++ src/components/AdminNav.vue
@@ -1,15 +1,9 @@
-<template>
-  <q-item
+<template><q-item
     clickable
     @click="routeClick(link)"><q-item-section
       v-if="icon"
       avatar
-    ><q-icon :name="icon" />
-    </q-item-section><q-item-section>
-      <q-item-label>{{ title }}</q-item-label>
-      <q-item-label caption>{{ caption }}</q-item-label>
-    </q-item-section>
-  </q-item>
+    ><q-icon :name="icon" /></q-item-section><q-item-section><q-item-label>{{ title }}</q-item-label><q-item-label caption>{{ caption }}</q-item-label></q-item-section></q-item>
 </template>
 
 <script>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -6,6 +6,17 @@ console.log("Username: "+authStore.username)
 </script>
 <template>
   <q-page class="flex flex-center">
-    <h1>Welcome {{authStore.username}}!</h1>
+
+    <div v-if="authStore.username === 'Guest'">
+      <!-- if true, render a link to login page -->
+      <p>Welcome, {{authStore.username}}</p>
+      Please <router-link to="/login">Login</router-link>
+    </div>
+    <div v-else>
+      <!-- if false, render a personalized message -->
+      <p>Welcome, {{authStore.username}}</p>
+      You have been authenticated
+    </div>
+
   </q-page>
 </template>
