a909af55caf3fc7c31e78fe3482db56ab9e6d44e
anthony krinsky <anthony.krinsky@databricks.com>
2024-01-29 23:34:30 -0800

    Updated to use fastAPI on backend

--- python_api/start_server.sh
+++
@@ -1,1 +0,0 @@
-python3 -m http.server -d ~/WebstormProjects/readmissions-quasar/readmissions-app/python_api/
--- src/boot/axios.js
+++ src/boot/axios.js
@@ -4,14 +4,14 @@ import axios from 'axios'
 const api = axios.create({ baseURL: 'https://localhost:8080' })
 
 export default boot(({ app }) => {
-  // for use inside Vue files (Options API) through this.$axios and this.$api
+  // for use inside Vue files (Options API) through this.$axios and this.$app
 
   app.config.globalProperties.$axios = axios
   // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
   //       so you won't necessarily have to import axios in each vue file
 
   app.config.globalProperties.$api = api
-  // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
+  // ^ ^ ^ this will allow you to use this.$app (for Vue Options API form)
   //       so you can easily perform requests against your app's API
 })
 
